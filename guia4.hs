467b9adbd4fbd5968dfe4630d2f2f6a5130582cb
sofiprovvi <sofiaprovvisionato7@gmail.com>
2024-04-17 13:53:13 -0300

    Hasta el 12 exclusive

--- guia4.hs
+++ guia4.hs
@@ -73,10 +73,67 @@ cantDigitos n
 
 -- ej 9
 
-esCapicua :: Integer -> Bool
+esCapicua :: Integer -> Bool -- ESTA MAL NO ANDA PARA 2034
 esCapicua n
     | cantDigitos n == 1 = True
     | cantDigitos n == 2 = iesimoDigito n 1 == iesimoDigito n 2
     | otherwise = (iesimoDigito n 1 == iesimoDigito n (cantDigitos n)) && esCapicua (div (mod n ((iesimoDigito n 1) * 10^((cantDigitos n)-1))) 10)
 
 -- esCapicua 7345 = False && esCapicua (div (mod n ((iesimoDigito n 1) * 10^((cantDigitos n)-1))) 10)
+-- esCapicua 2032 = True && esCapicua (div (mod n (2000) 10)
+
+-- ej 10
+-- a
+
+f1 :: Integer -> Integer
+f1 0 = 1
+f1 n = 2^n + f1 (n-1)
+
+-- b 
+
+f2 :: Integer -> Float -> Float
+f2 1 q = q^1
+f2 n q = q^n + f2 (n-1) q
+
+-- f2 2 3.4 = 3.4^2 + 3.4^1
+
+-- c
+
+f3 :: Integer -> Float -> Float
+f3 0 q = 0
+f3 n q = q^(n*2) + q^(n*2 - 1) + f3 (n-1) q
+
+
+-- f3 0 3.4 = 0 (conjunto vacio)
+-- f3 1 3.4 = 3.4^1 + 3.4^2
+-- f3 2 3.4 = 3.4^4 + 3.4^3 + 3.4^1 + 3.4^2
+-- f3 3 3.4 = 3.4^6 + 3.4^5 + 3.4^4 + 3.4^3 + 3.4^1 + 3.4^2
+
+-- d
+
+f4 :: Integer -> Float -> Float
+f4 0 q = q^0
+f4 n q = q^(n*2) + q^(n*2 - 1) + f4 (n-1) q - q^(n-1)
+
+-- f4 0 3.4 = 3.4 ^ 0 = 1
+-- f4 1 3.4 = 3.4^2 + 3.4^1
+-- f4 2 3.4 = 3.4^4 + 3.4^3 + 3.4^2
+-- f4 3 3.4 = 3.4^6 + 3.4^5 + 3.4^4 + 3.4^3
+-- f4 4 3.4 = 3.4^8 + 3.4^7 + 3.4^6 + 3.4^5 + 3.4^4
+
+-- ej 11
+-- a
+eAprox :: Integer -> Float
+eAprox 0 = 1 / fromIntegral (factorial 0)
+eAprox n = (1 / fromIntegral (factorial n)) + eAprox (n-1)
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+-- b
+e :: Float
+e = eAprox 9
+
+-- ej 12
+raizDe2Aprox :: Integer -> Float
